d04442ba9a83bbe45656b89ef184ed7120a5f76d
unknown <54745imannol@gmail.com>
2019-05-31 10:20:33 -0600

    Ejemplo-Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exam-angular';
+  
+name: string = 'Jhon Carter';
+age: number = 28;
+
+  users: string[] = ['imannol','mariano','fernando','antonio','chepe'];
+  
+  constructor(private dataService: DataService){
+    this.dataService.getData().subscribe(data =>{
+    this.posts = data;
+    });
+  }
+   
+  addUser(newUser){
+    this.users.push(newUser.value);
+    newUser.value = '';
+    newUser.focus();
+    return false;
+  }
+
+  deleteUser(user){
+  for(let i = 0; i < this.users.length; i++){
+    if(user == this.users[i]){
+      this.users.splice(i,1)
+    }
+  }
+ }
+
+ posts = [];
+
 }
+  
